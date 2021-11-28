3a891f41a4f081727c1a92bbd2a1100de8411eae
Nolan Kurylo <44009838+nolankurylo@users.noreply.github.com>
2021-11-28 00:12:23 -0800

    first working ai model

--- modelDeployment/app.py
+++ modelDeployment/app.py
@@ -1,30 +1,32 @@
 import base64
 import json
 import numpy as np
-import tensorflow as tf
 from keras.models import load_model
 from PIL import Image
 from io import BytesIO
-import matplotlib.pyplot as plt
 
-model = load_model('/var/task/basic_mnist_nn.h5')
+
+model = load_model('/var/task/model.h5')
 
 
 def lambda_handler(event, context):
-    print("helllooooo")
-    print(event['body'])
     image_bytes = event['body'].encode('utf-8')
-    print("2")
     image = Image.open(BytesIO(base64.b64decode(image_bytes))).convert(mode='L')
     print("3")
     image = image.resize((28, 28))
-    plt.imshow(image)
+    np_img = np.array(image) / 255.0
 
-    probabilities = model(np.array(image).reshape(-1, 28, 28, 1))
+    probabilities = model(np_img.reshape(-1, 28, 28, 1))
     label = np.argmax(probabilities)
 
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+
+        },
         'body': json.dumps(
             {
                 "predicted_label": int(label),
--- modelTraining/modelTraining.py
+++ modelTraining/modelTraining.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, InputLayer, Conv2D, Flatten, MaxPooling2D
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, InputLayer, Conv2D, Flatten, MaxPooling2D, Dense
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.optimizers import SGD, Adam
@@ -34,31 +34,25 @@ class ModelTraining:
         print(X_train.shape)
         model = Sequential([
             InputLayer(input_shape=(28, 28, 1)),  # input layer
-            Conv2D(16, (3,3), activation='relu'),  # convolutional layer
-            MaxPooling2D((2,2)),  # pooling layer
-            # Conv2D(32, (3, 3), activation='relu'),  # convolutional layer
-            # MaxPooling2D((2, 2)),  # pooling layer
-            # Conv2D(64, (3, 3), activation='relu'),  # convolutional layer
-            # MaxPooling2D((2, 2)),  # pooling layer
-            # Conv2D(64, (3, 3), activation='relu'),  # convolutional layer
-            # MaxPooling2D((2, 2)),  # pooling layer
-            # Conv2D(64, (3, 3), activation='relu'),  # convolutional layer
-            # MaxPooling2D((2, 2)),  # pooling layer
+            Conv2D(32, (3,3), activation='relu'),  # convolutional layer
+            # MaxPooling2D((2,2)),  # pooling layer
             Flatten(),  # reshape
-            Dense(100, activation='relu', kernel_initializer='he_uniform'),  # fully connected layer
-            # Dense(64, activation='relu'),  # fully connected layer
-
-
+            # Dense(256, activation='relu'),  # fully connected layer
+            # Dropout(0.4),
+            # BatchNormalization(),
+            Dense(128, activation='relu'),  # fully connected layer
+            # Dropout(0.4),
+            # BatchNormalization(),
             Dense(10, activation="softmax")  # output layer
         ])
         print(model.summary())
 
-        opt = Adam(lr=3e-5)
-        # opt = SGD(lr=0.01, momentum=0.9)
+        # opt = Adam(lr=3e-5)
+        opt = SGD()
 
         es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
         model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-        model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=20, batch_size=64, callbacks=[es])
+        model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=64, callbacks=[es])
         return model
 
 
